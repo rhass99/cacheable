ac269494eca27b5213cc2ca70ebf53307ff43155
Jonathan Liu <jxliu95@gmail.com>
2019-06-20 12:22:02 +0000

    like toggle, updating post rating

--- server/db/helpers/post.js
+++ server/db/helpers/post.js
@@ -7,10 +7,8 @@ export const makePostHelpers = (knex) => {
         url: newPost.url,
         title: newPost.title,
         description: newPost.description,
-        img: newPost.img
-        // we're doing something special with rating, so
-        // this shouldn't be here, right?
-        // rating: newPost.rating
+        img: newPost.img,
+        rating: null
       }).asCallback((err) => {
         if (err) return console.error(err);
         cb(null, true);
@@ -28,6 +26,17 @@ export const makePostHelpers = (knex) => {
       } else {
         knex.select().from('post').limit(value);
       }
+    },
+
+    updateRating: (rating, post_id, cb) => {
+      const currentRating = knex.select('rating').from('post').where('post_id', post_id).asCallback((err, result) => {
+        if (err) return console.error(err);
+        return Number(result[0]);
+      });
+      knex('post').where('post_id', post_id).update({rating: currentRating + Number(rating)}).asCallback((err) => {
+        if (err) return console.error(err);
+        cb(null, true);
+      });
     }
   };
 };
