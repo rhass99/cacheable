b7c2df113e55d3773b627c92c73df7148041b2ff
Jonathan Liu <jxliu95@gmail.com>
2019-06-20 02:44:04 +0000

    post helper functions

---
+++ server/db/helpers/post.js
@@ -0,0 +1,31 @@
+export const makePostHelpers = (knex) => {
+  return {
+    // Saves post to db
+    savePost: (newPost, cb) => {
+      knex('post').insert({
+        user_id: newPost.user_id,
+        url: newPost.url,
+        title: newPost.title,
+        description: newPost.description,
+        img: newPost.img,
+        rating: newPost.rating
+      }).asCallback((err) => {
+        if (err) return console.error(err);
+        cb(null, true);
+      });
+    },
+
+    // Get posts from db, either searching by something or
+    // nonspecific with a limit
+    getPosts: (value, property, cb) => {
+      if (property) {
+        knex.select().from('post').where(property, value).asCallback((err) => {
+          if (err) return console.error(err, result);
+          cb(null, result);
+        });
+      } else {
+        knex.select().from('post').limit(value);
+      }
+    }
+  };
+}
