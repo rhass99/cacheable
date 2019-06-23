456715eecbbc80700179cf6cc11d23947c0924f7
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-23 13:59:55 -0400

    Make posts dynamic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,9 @@ $( document ).ready(function() {
   $('.like').on ('click', function (e) {
     $(this).toggleClass('liked');
   });
+  // for (let like of likes) {
+  //   if (like.user_id === user_id) {
+  //     $(`*[data-post-id=${like.post_id}]`).addClass('liked');
+  //   }
+  // }
 });
