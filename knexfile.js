fcf351a42188dea2ddb39b1b34dc7512a588ce87
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-23 18:59:40 -0400

    Fix routes

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,7 @@ module.exports = {
       user     : process.env.DB_USER,
       password : process.env.DB_PASS,
       database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      port     : process.env.DB_PORT
     },
     pool: {
       min: 1,
