694406e28e320756f49503b4a38f1e5405a3fb65
Jonathan Liu <jxliu95@gmail.com>
2019-06-24 00:07:41 +0000

    hide and show by tag, first step

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,15 @@ $( document ).ready(function() {
   $('.like').on ('click', function (e) {
     $(this).toggleClass('liked');
   });
+
+  $(`#all`).on ('click', function (e) {
+    $(`article`).show();
+  });
+
+  $(`#bootstrap`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='bootstrap']`).show();
+  });
   // for (let like of likes) {
   //   if (like.user_id === user_id) {
   //     $(`*[data-post-id=${like.post_id}]`).addClass('liked');
