e39fae6c70c55da24b0ff6d04b1ede9bee8e17a0
Jonathan Liu <jxliu95@gmail.com>
2019-06-24 00:51:54 +0000

    tags and ratings

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,13 +4,93 @@ $( document ).ready(function() {
     $(this).toggleClass('liked');
   });
 
+  $('.rate1').on ('click', function (e) {
+    $(this).toggleClass('checked');
+    $(this).siblings('.rate2').removeClass('checked');
+    $(this).siblings('.rate3').removeClass('checked');
+    $(this).siblings('.rate4').removeClass('checked');
+    $(this).siblings('.rate5').removeClass('checked');
+  });
+
+  $('.rate2').on ('click', function (e) {
+    $(this).siblings('.rate1').addClass('checked');
+    $(this).toggleClass('checked');
+    $(this).siblings('.rate3').removeClass('checked');
+    $(this).siblings('.rate4').removeClass('checked');
+    $(this).siblings('.rate5').removeClass('checked');
+  });
+
+  $('.rate3').on ('click', function (e) {
+    $(this).siblings('.rate1').addClass('checked');
+    $(this).siblings('.rate2').addClass('checked');
+    $(this).toggleClass('checked');
+    $(this).siblings('.rate4').removeClass('checked');
+    $(this).siblings('.rate5').removeClass('checked');
+  });
+
+  $('.rate4').on ('click', function (e) {
+    $(this).siblings('.rate1').addClass('checked');
+    $(this).siblings('.rate2').addClass('checked');
+    $(this).siblings('.rate3').addClass('checked');
+    $(this).toggleClass('checked');
+    $(this).siblings('.rate5').removeClass('checked');
+  });
+
+  $('.rate5').on ('click', function (e) {
+    $(this).siblings('.rate1').addClass('checked');
+    $(this).siblings('.rate2').addClass('checked');
+    $(this).siblings('.rate3').addClass('checked');
+    $(this).siblings('.rate4').addClass('checked');
+    $(this).toggleClass('checked');
+  });
+
   $(`#all`).on ('click', function (e) {
     $(`article`).show();
+  });
+
+  $(`#angular`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='angular']`).show();
   });
 
   $(`#bootstrap`).on ('click', function (e) {
     $(`article`).hide();
     $(`[data-tag='bootstrap']`).show();
+  });
+
+  $(`#css`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='css']`).show();
+  });
+
+  $(`#html`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='html']`).show();
+  });
+
+  $(`#js`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='js']`).show();
+  });
+
+  $(`#ruby`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='ruby']`).show();
+  });
+
+  $(`#sql`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='sql']`).show();
+  });
+
+  $(`#jquery`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='jquery']`).show();
+  });
+
+  $(`#react`).on ('click', function (e) {
+    $(`article`).hide();
+    $(`[data-tag='react']`).show();
   });
   // for (let like of likes) {
   //   if (like.user_id === user_id) {
